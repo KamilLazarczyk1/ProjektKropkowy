a582b1e626d3ba32eb2b7e69377e01f27698d28c
KamilLazarczyk <103037577+KamilLazarczyk@users.noreply.github.com>
2024-03-06 15:55:07 +0100

    hgfh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mainCanvas = document.querySelector("canvas")
 const ctx = mainCanvas.getContext("2d")
 ctx.fillStyle = "black"
 const dotCount = prompt("ile kropek")
-let dots = [];
+let dots = [], mainDot1, mainDot2;
 function drawDots(ctx, dotCount)
 {
     for(i = 0; i < dotCount; i++)
@@ -11,6 +11,11 @@ function drawDots(ctx, dotCount)
         y = Math.floor(Math.random() * 798) + 1;
         dots[i] = ctx.fillRect(x, y, 3, 3);
     }
+
+    //wybieranie 2 kropek
+    mainDot1 = dots[Math.floor(Math.random() * dotCount)]
+    mainDot2 = dots[Math.floor(Math.random() * dotCount)]
 }
+
 
 drawDots(ctx, dotCount)
