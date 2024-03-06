7390b6a3e99f2c9a82c04206182d946851c2dbd3
KamilLazarczyk <103037577+KamilLazarczyk@users.noreply.github.com>
2024-03-06 16:21:50 +0100

    jhgjh

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const mainCanvas = document.querySelector("canvas")
 const ctx = mainCanvas.getContext("2d")
-ctx.fillStyle = "black"
 const dotCount = prompt("ile kropek")
-let dots = [], mainDot1, mainDot2;
+const dotWidth = 5, dotHeight = 5
+let dots = [], mainDot1, mainDot2
 function drawDots(ctx, dotCount)
 {
+    ctx.fillStyle = "black"
     for(i = 0; i < dotCount; i++)
     {
         x = Math.floor(Math.random() * 758) + 20;
         y = Math.floor(Math.random() * 758) + 20;
-        dots[i] = ctx.fillRect(x, y, 3, 3);
+        ctx.fillRect(x, y, dotWidth, dotHeight);
+        dots[i] = {x: x, y: y};
     }
 
     //wybieranie 2 kropek
+    ctx.fillStyle = "red"
     mainDot1 = dots[Math.floor(Math.random() * dotCount)]
-    mainDot2 = dots[Math.floor(Math.random() * dotCount)]
+    console.log(dots)
+    do {
+        mainDot2 = dots[Math.floor(Math.random() * dotCount)]
+    } while(mainDot1 == mainDot2)
+    ctx.fillRect(mainDot1.x, mainDot1.y, dotWidth, dotHeight);
+    ctx.fillRect(mainDot2.x, mainDot2.y, dotWidth, dotHeight);
 }
 
 
