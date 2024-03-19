453c5d54ec6356eadc60a7fc0e258fb6efd881c1
Kamil Łazarczyk <pt20lazarczykk@zsi.kielce.pl>
2024-03-19 11:01:30 +0100

    dziala3

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const mainCanvas = document.querySelector("canvas")
 const ctx = mainCanvas.getContext("2d")
-let dots = [], selectedDots = [], wayFound = false, dotCount, mainDot1, mainDot2
+let dots = [], selectedDots = [], wayFound = false, dotCount, mainDot1, mainDot2, startTime, pathDistance = 0;
 do {
     dotCount = parseInt(prompt("ile kropek"))
-} while (dotCount < 2)
+} while (dotCount < 50)
 const dotWidth = 5, dotHeight = 5
 ctx.lineWidth = 2;
+
+function startTimer() {
+    startTime = new Date();
+}
+
+function stopTimer() {
+    let endTime = new Date();
+    let timeDiff = endTime - startTime;
+    console.log(`Trwało to ${timeDiff} milisekund.`);
+}
+
+
 function drawDots(ctx, dotCount) {
     ctx.fillStyle = "black"
     for (i = 0; i < dotCount; i++) {
@@ -17,7 +29,6 @@ function drawDots(ctx, dotCount) {
 
     //wybieranie 2 kropek
     mainDot1 = dots[Math.floor(Math.random() * dotCount)]
-    console.log(dots)
     do {
         mainDot2 = dots[Math.floor(Math.random() * dotCount)]
     } while (mainDot1.x == mainDot2.x && mainDot1.y == mainDot2.y)
@@ -48,19 +59,18 @@ function findDotsInBetween(dot1, dot2, cDots) {
     return newDots;
 }
 function findTheWay() {
-    let firstDot = { x: mainDot1.x, y: mainDot1.y }, closestDot = { x: mainDot2.x, y: mainDot2.y }
+    let firstDot = { x: mainDot1.x, y: mainDot1.y }, closestDot = { x: mainDot2.x, y: mainDot2.y }, distElToFirst, distFirstToClosest
     selectedDots = findDotsInBetween(mainDot1, mainDot2, dots)
     do {
-        console.log("dowhile" + selectedDots)
         selectedDots.forEach(el => {
-            const distElToFirst = Math.sqrt(Math.pow((el.x - firstDot.x), 2) + Math.pow((el.y - firstDot.y), 2))
-            const distFirstToClosest = Math.sqrt(Math.pow((closestDot.x - firstDot.x), 2) + Math.pow((closestDot.y - firstDot.y), 2))
+            distElToFirst = Math.sqrt(Math.pow((el.x - firstDot.x), 2) + Math.pow((el.y - firstDot.y), 2))
+            distFirstToClosest = Math.sqrt(Math.pow((closestDot.x - firstDot.x), 2) + Math.pow((closestDot.y - firstDot.y), 2))
             if (distElToFirst < distFirstToClosest) {
                 closestDot = { x: el.x, y: el.y }
             }
-            console.log(closestDot)
         });
         ctx.fillStyle = "white"
+        pathDistance += distFirstToClosest;
         ctx.beginPath();
         ctx.moveTo(firstDot.x, firstDot.y);
         ctx.lineTo(closestDot.x, closestDot.y);
@@ -75,11 +85,15 @@ function findTheWay() {
         firstDot = { x: closestDot.x, y: closestDot.y }, closestDot = { x: mainDot2.x, y: mainDot2.y }
     } while (wayFound == false && selectedDots.length > 0);
     ctx.fillStyle = "white"
+    pathDistance += Math.sqrt(Math.pow((mainDot2.x - firstDot.x), 2) + Math.pow((mainDot2.y - firstDot.y), 2));
     ctx.beginPath();
     ctx.moveTo(firstDot.x, firstDot.y);
     ctx.lineTo(mainDot2.x, mainDot2.y);
     ctx.stroke();
+    console.log(`Przebyta droga (w pixelach) = ${pathDistance}`)
 }
 
+startTimer()
 drawDots(ctx, dotCount)
 findTheWay()
+stopTimer()
